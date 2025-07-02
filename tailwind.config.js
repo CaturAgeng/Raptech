f337ee4a913788e9f3226d8250961b174f1ba607
CaturAgeng <caturageng023@gmail.com>
2025-07-02 08:33:07 +0700

    Modified configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+// module.exports = {
+//     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: [
+        './app/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: 'hsl(var(--primary))',
+                // dst...
+            },
+        },
     },
     plugins: [],
-};
+}
